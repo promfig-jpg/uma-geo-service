0dcdff885aaf681c632af1853be0136ae0c19798
promfig-jpg <promfig@gmail.com>
2026-07-22 12:24:31 +0200

    Update geo.py

--- app/routers/geo.py
+++ app/routers/geo.py
@@ -3,6 +3,8 @@ from fastapi import APIRouter, HTTPException
 from app.models.geo import GeoRequest
 from app.services.h3_service import coordinates_to_h3
 from app.services.geocoding_service import reverse_geocode
+from app.services.osm_service import get_way_details
+from app.services.street_service import calculate_street_importance
 
 
 router = APIRouter(prefix="/geo", tags=["Geo"])
@@ -22,12 +24,31 @@ async def enrich(data: GeoRequest):
             data.longitude
         )
 
+        highway_type = None
+        street_importance = 0
+        osm_way = None
+
+        osm_id = geocoding_data.get("osm_id")
+        osm_type = geocoding_data.get("osm_type")
+
+        if osm_id and osm_type == "way":
+            osm_way = await get_way_details(osm_id)
+
+            highway_type = osm_way.get("highway")
+
+            street_importance = calculate_street_importance(
+                highway_type
+            )
+
         return {
             "success": True,
             "latitude": data.latitude,
             "longitude": data.longitude,
             **h3_data,
-            **geocoding_data
+            **geocoding_data,
+            "highway_type": highway_type,
+            "street_importance": street_importance,
+            "osm_way": osm_way
         }
 
     except Exception as error:
