e70cfd685c6e9676246c0e2c5e587b5d86819181
promfig-jpg <promfig@gmail.com>
2026-07-22 13:25:39 +0200

    Update geo.py

--- app/routers/geo.py
+++ app/routers/geo.py
@@ -4,6 +4,7 @@ from app.models.geo import GeoRequest
 from app.services.h3_service import coordinates_to_h3
 from app.services.geocoding_service import reverse_geocode
 from app.services.street_service import calculate_street_importance
+from app.cache.memory_cache import get_cache, set_cache
 
 
 router = APIRouter(prefix="/geo", tags=["Geo"])
@@ -18,15 +19,44 @@ async def enrich(data: GeoRequest):
             data.resolution
         )
 
+        h3_index = h3_data["h3_index"]
+
+        cached_data = get_cache(h3_index)
+
+        if cached_data:
+            return {
+                "success": True,
+                "latitude": data.latitude,
+                "longitude": data.longitude,
+                **h3_data,
+                **cached_data,
+                "cache": {
+                    "hit": True
+                }
+            }
+
         geocoding_data = await reverse_geocode(
             data.latitude,
             data.longitude
         )
 
-        highway_type = geocoding_data.get("street_type")
+        highway_type = geocoding_data.get(
+            "street_type"
+        )
 
         street_importance = calculate_street_importance(
             highway_type
+        )
+
+        geo_data = {
+            **geocoding_data,
+            "highway_type": highway_type,
+            "street_importance": street_importance
+        }
+
+        set_cache(
+            h3_index,
+            geo_data
         )
 
         return {
@@ -34,9 +64,10 @@ async def enrich(data: GeoRequest):
             "latitude": data.latitude,
             "longitude": data.longitude,
             **h3_data,
-            **geocoding_data,
-            "highway_type": highway_type,
-            "street_importance": street_importance
+            **geo_data,
+            "cache": {
+                "hit": False
+            }
         }
 
     except Exception as error:
