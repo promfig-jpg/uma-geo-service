25b00ea1629ca69f0a857d998f637390576f6cf8
promfig-jpg <promfig@gmail.com>
2026-07-22 13:47:22 +0200

    Create poi.py

---
+++ app/routers/poi.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, HTTPException
+
+from app.models.geo import GeoRequest
+from app.services.poi_service import get_nearby_pois
+
+
+router = APIRouter(prefix="/pois", tags=["POIs"])
+
+
+@router.post("")
+async def pois(data: GeoRequest):
+    try:
+        result = await get_nearby_pois(
+            data.latitude,
+            data.longitude
+        )
+
+        return {
+            "success": True,
+            "latitude": data.latitude,
+            "longitude": data.longitude,
+            **result
+        }
+
+    except Exception as error:
+        raise HTTPException(
+            status_code=400,
+            detail=str(error)
+        )
